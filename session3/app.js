79f7777d80e7fe877774f43684d443d06f5e009e
karamjeet1608 <singh.karamjeet1@gmail.com>
2019-04-17 10:58:13 +0200

    working with Templeting engines
    Implementation of templeting engines like
    
    pug
    express- handlebars
    ejs

---
+++ session3/app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//const expressHbs = require('express-handlebars');
+
+const app = express();
+
+//app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/',defaultLayout: 'main-layout'}));
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);    
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
+});
+
+app.listen(3100);
---
+++ session3/routes/shop.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+  //console.log('shopjs', adminData.products);
+  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  //res.render('shop', {prods: products, docTitle: 'Shop', path: '/'});
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+
+});
+
+module.exports = router;
